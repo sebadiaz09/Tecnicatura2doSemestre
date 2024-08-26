0bf7b01ff4c041df579924515d665c77dc427685
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-26 00:46:41 -0300

    Vemos ciclo do while

--- java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -12,6 +12,12 @@ public class EjercicioWhile01 {
             System.out.println("conteo = " + conteo);
             conteo++; // Vamos aumentando en uno la variable
         }
+        
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador++;
+        }while(contador < 7);
     }
     
 }
