cfc5e8e05afa14dc80e09e8fa0b8f98341e23bf0
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-26 00:39:06 -0300

    Comenzamos segundo semestre java con ciclowhile

---
+++ java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -0,0 +1,17 @@
+
+package CicloWhile;
+
+/**
+ *
+ * @author Seba
+ */
+public class EjercicioWhile01 {
+    public static void main(String[] args) {
+        var conteo = 0; // Inferencia de tipos
+        while(conteo <= 7){
+            System.out.println("conteo = " + conteo);
+            conteo++; // Vamos aumentando en uno la variable
+        }
+    }
+    
+}
