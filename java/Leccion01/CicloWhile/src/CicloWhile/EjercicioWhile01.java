e1ef9a5c8b1b4fecec7947e507ff82641c9da313
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-26 22:14:26 -0300

    Vemos ciclo for

--- java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -8,7 +8,7 @@ package CicloWhile;
 public class EjercicioWhile01 {
     public static void main(String[] args) {
         var conteo = 0; // Inferencia de tipos
-        while(conteo <= 7){
+        while(conteo <= 7) {
             System.out.println("conteo = " + conteo);
             conteo++; // Vamos aumentando en uno la variable
         }
@@ -18,6 +18,10 @@ public class EjercicioWhile01 {
             System.out.println("contador = " + contador);
             contador++;
         }while(contador < 7);
+        
+        for(var contando = 0; contando < 7; contando++){
+            System.out.println("contando = " + contando);
+        }
     }
-    
 }
+
