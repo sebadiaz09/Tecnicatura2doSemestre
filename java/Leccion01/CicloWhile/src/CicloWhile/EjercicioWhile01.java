1c771f4f3a83201fb59404b1b47631a5a5a4810f
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-26 22:39:15 -0300

    Vemos palabras breake y continue

--- java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
+++ java/Leccion01/CicloWhile/src/CicloWhile/EjercicioWhile01.java
@@ -20,7 +20,19 @@ public class EjercicioWhile01 {
         }while(contador < 7);
         
         for(var contando = 0; contando < 7; contando++){
-            System.out.println("contando = " + contando);
+           if(contando % 2 == 0){
+             System.out.println("contando = " + contando);  
+             //break;
+           }
+           
+        }
+        
+        for(var contando = 0; contando < 7; contando++){
+           if(contando % 2 != 0) {
+              continue; //Vamos a la siguiente iteración
+           
+           }
+           System.out.println("contando = " + contando); 
         }
     }
 }
