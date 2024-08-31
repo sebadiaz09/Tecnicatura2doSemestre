eb2e524041c32ac8d50b1f1d28d3997b96948aa9
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-31 20:38:47 -0300

    Modificamos los elementos

--- javascript/2SLeccion02/02-03.areglos.js
+++ javascript/2SLeccion02/02-03.areglos.js
@@ -11,3 +11,19 @@ console.log(autos[2]);
 for(let i = 0; 1 < autos.length; i++) {
     console.log(i+' : '+autos[i]);
 }
+
+// Modificamos los elementos del arreglo
+autos[1] = 'Volvo';
+console.log(autos[1]);
+
+// Agregamos nuevos valores al arreglo
+autos.push('Audi'); // Agregamos el elemento al final del arreglo
+console.log(autos);
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.lenght] = 'Porche';
+console.log(autos);
+
+// Tercera forma de agregar elementos teniendo cuidado
+autos[6] = 'Renault';
+console.log(autos);
