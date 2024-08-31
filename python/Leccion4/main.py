ec3a2393a3cbe108947c8136bd41a77f8ec95520
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-31 20:20:20 -0300

    Repasamos y vemos mas conceptos de Listas

--- python/Leccion4/main.py
+++ python/Leccion4/main.py
@@ -207,3 +207,10 @@ lista3.sort() # Ordena los elementos ascendentemente
 print(lista3)
 lista3.sourt(reverse=True) # Ordena descendentemente
 print(lista3)
+
+tupla = (4, 'Hola', 6.78, [1, 2, 78], 4, 'Hola' ) # Puede tener diferentes tipos de datos dentro
+print(tupla)
+
+print(4 in tupla) # Acción booleana, su respuesta es de tipo booleana
+# Lo que podemos usar dentro de tuplas son: index, count, len
+# En tuplas se puede convertir de tupla a lista y de lista a tupla
