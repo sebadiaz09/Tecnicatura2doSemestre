6f58f9ebd5690c727436ebce84efcd5a5ba9b5e1
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-08-30 23:45:01 -0300

    Vemos diccionario en Python

--- python/Leccion4/main.py
+++ python/Leccion4/main.py
@@ -108,9 +108,67 @@ print(planetas)
 planetas.discard('Tierra') # Esta función no nos presenta ningun error
 print(planetas)
 
-# Limpiar set
+# Limpiar set o conjunto
 planetas.clear()
 print(planetas)
 
-# Eliminar set
+# Eliminar set o conjunto
 del planetas
+print(planetas) # al eliminar muestra fun error
+
+# 'Maradona':10 Un diccionario estas compuesto por dos elementos
+# UNA LLAVE Y UN VALOR
+# dict(key,value)
+diccionario = {
+    'IDE':'Integrated Development Environment',
+    'POO':'Programación Orientada a Objetos',
+    'SABD':'Sistema de Administración de Base de Datos'
+}
+# Verificamos la cantidad de elementos del diccionario
+print(len(diccionario))
+print(diccionario)
+
+
+# Acceder a un diccionario con la llave(key)
+print(diccionario['IDE'])
+
+# Otra forma de recuperar un elemento
+print(diccionario.get('POO'))
+print(diccionario.get('SABD'))
+
+# Modificamos elementos
+diccionario['IDE'] = 'Entorno de Desarrollo Integrado'
+print(diccionario)
+
+# Como recorrer los elementos
+for termino in diccionario: # Recorremos mostrando solo las llaves
+    print(termino)
+
+# Necesitamos una funcón para recorrer un diccionario
+for termino, valor in diccionario.items():
+    print(termino, valor)
+
+# Otras maneras de acceder a un diccionario
+for termino in diccionario.keys():
+    print(termino) # Muestra solo las llaves
+
+for valor in diccionario.values(): # Usamos una función para acceder al valor
+    print(valor)
+
+# Comprobar la existencia de algún elemento
+print('IDE' in diccionario) # Devuelve un booleano
+
+# Agregar un elemento
+diccionario['PK'] = 'Primary key'
+print(diccionario)
+
+# Eliminar un elemento
+diccionario.pop('SABD')
+print(diccionario)
+
+# Vaciar un diccionario
+diccionario.clear()
+print(diccionario)
+
+# Elimianr diccionario
+del diccionario # El diccionario se borró
